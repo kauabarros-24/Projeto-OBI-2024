2a1a1af97080abef1cff34526778d64e954183da
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-01-27 12:40:09 -0300

    Estamos realizando um exercício de ordenação com filas

---
+++ Estruturas de dados/copa.py
@@ -0,0 +1,23 @@
+from queue import SimpleQueue
+import string
+
+a = list(string.ascii_lowercase)
+q = SimpleQueue()
+
+for c in a:
+    q.put(c)
+
+#Programa principal:
+
+for i in range(8):
+    resultado = input("Digite o resultado do jogo: ").split()
+    for n in resultado:
+        pass
+
+
+
+    
+       
+    
+    
+
