b25f2a1a80290b76ac30765d487dd9505ce3e218
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-01-28 19:20:09 -0300

    Implementamos um método para as semifinais

--- Estruturas de dados/copa.py
+++ Estruturas de dados/copa.py
@@ -1,27 +1,26 @@
 from queue import SimpleQueue
 import string
 
-#ÁREA DE VAR'S
+# ÁREA DE VARIAVEIS
 a = list(string.ascii_lowercase)
 q = SimpleQueue()
 listaOitavas = []
+listaQuartas = []
+listaSemi = []
 n = 1
 
-#Adicionais:
-for c in a:
-    q.put(a)
-    if q.qsize() == 16:
-        break
+#Extra:
+for i in a:
+    q.put(i)
 print(q.qsize())
-    
-#Tratamento:
+
+#Tratamento
 class Pontos:
     def __init__(self, resultado0, resultado1):
         self.resultado0 = resultado0
         self.resultado1 = resultado1
-
-    while n <= 12:
-        if n <= 8 :
+    while n <= 14:
+        if n <= 8:
             def oitavas(self):
                 if self.resultado0 > self.resultado1:
                     q.get(a[1])
@@ -34,24 +33,44 @@ class Pontos:
                     a.remove(a[0])
                     a.remove(a[0])
                 print(listaOitavas)
-        elif n >=9:
-                def quartas(self):
-                    if self.resultado0 > self.resultado1:
-                        q.get(listaOitavas[1])
-                        listaOitavas.remove(listaOitavas[1])
-                        print(listaOitavas)
-                    else:
-                        q.get(listaOitavas[0])
-                        listaOitavas.remove(listaOitavas[0])
-                        print(listaOitavas)
+        elif n <= 12:
+            def quartas(self):
+                if self.resultado0 > self.resultado1:
+                    q.get(listaOitavas[1])
+                    listaQuartas.append(listaOitavas[0])
+                    listaOitavas.remove(listaOitavas[0])
+                    listaOitavas.remove(listaOitavas[0])
+                else:
+                    q.get(listaOitavas[0])
+                    listaQuartas.append(listaOitavas[1])
+                    listaOitavas.remove(listaOitavas[0])
+                    listaOitavas.remove(listaOitavas[0])
+                print(listaQuartas)
+        elif n <= 14:
+            def semi(self):
+                if self.resultado0 > self.resultado1:
+                    q.get(listaQuartas[1])
+                    listaSemi.append(listaQuartas[0])
+                    listaQuartas.remove(listaQuartas[0])
+                    listaQuartas.remove(listaQuartas[0])
+                    print(listaSemi)
+                else:
+                    q.get(listaQuartas[0])
+                    listaSemi.append(listaQuartas[1])
+                    listaQuartas.remove(listaQuartas[0])
+                    listaQuartas.remove(listaQuartas[0])
+                    print(listaSemi)
+                print(listaSemi)
         n+=1
 
-#Programa principal:
-for i in range(12):
-    ##Pegar valores:
-    resultado = input("Digite os valores da partida: ").split()
+#Programa principal
+
+for i in range(14):
+    resultado = input("Digite os valores: ").split()
     pontos = Pontos(resultado[0], resultado[1])
-    #pontos.oitavas()
-    if i > 8:
+    if i < 8:
+        pontos.oitavas()
+    elif i < 12:
         pontos.quartas()
-    
+    elif i < 14:
+        pontos.semi()
