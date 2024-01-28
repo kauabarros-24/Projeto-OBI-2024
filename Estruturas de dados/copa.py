8fe0ccd4c1b30004635385880bd73d46c8f3f6db
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-01-28 18:47:01 -0300

    Criamos métodos para lidarmos com as oitavas e as quartas

--- Estruturas de dados/copa.py
+++ Estruturas de dados/copa.py
@@ -1,63 +1,56 @@
 from queue import SimpleQueue
 import string
 
+#ÁREA DE VAR'S
 a = list(string.ascii_lowercase)
 q = SimpleQueue()
-listaResult = []
-listaResult4 = []
-o = 0
-listaVencedores = []
+listaOitavas = []
+n = 1
 
+#Adicionais:
 for c in a:
-    q.put(c)
-    o+=1
-    if o == 16:
+    q.put(a)
+    if q.qsize() == 16:
         break
+print(q.qsize())
+    
+#Tratamento:
+class Pontos:
+    def __init__(self, resultado0, resultado1):
+        self.resultado0 = resultado0
+        self.resultado1 = resultado1
+
+    while n <= 12:
+        if n <= 8 :
+            def oitavas(self):
+                if self.resultado0 > self.resultado1:
+                    q.get(a[1])
+                    listaOitavas.append(a[0])
+                    a.remove(a[0])
+                    a.remove(a[0])
+                else:
+                    q.get(a[0])
+                    listaOitavas.append(a[1])
+                    a.remove(a[0])
+                    a.remove(a[0])
+                print(listaOitavas)
+        elif n >=9:
+                def quartas(self):
+                    if self.resultado0 > self.resultado1:
+                        q.get(listaOitavas[1])
+                        listaOitavas.remove(listaOitavas[1])
+                        print(listaOitavas)
+                    else:
+                        q.get(listaOitavas[0])
+                        listaOitavas.remove(listaOitavas[0])
+                        print(listaOitavas)
 
 #Programa principal:
-
-#Oitavas
-for i in range(8):
-    #Pegamos os resultados
-    resultado = input("Digite o resultado do jogo: ").split()
-    #Ordenamos eles
-    for n in resultado:
-        listaResult.append(n)
-    #Colocamos na fila, separando-os em listas:
-    if listaResult[0] > listaResult[1]:
-        q.get(a[1])
-        listaVencedores.append(a[0])
-        a.remove(a[0])
-        a.remove(a[0])
-        #print(listaVencedores)
-
-    else:
-        q.get(a[0])
-        listaVencedores.append(a[1])
-        a.remove(a[0])
-        a.remove(a[0])
-        #print(listaVencedores)
-        
-
-#Ganhadores das quartas:
-for p in range(4):
-    goleadas = input("Digite os resultados das quartas").split()
-    for c in goleadas:
-        listaResult4.append(c)
-    if listaResult4[0] > listaResult[1]:
-        q.get(listaVencedores[1])
-        listaVencedores.remove(listaVencedores[1])
-        print(listaVencedores)
-        
-    else:
-        listaVencedores.remove(listaVencedores[0])
-        q.get(listaVencedores[0])
-        print(listaVencedores)
+for i in range(12):
+    ##Pegar valores:
+    resultado = input("Digite os valores da partida: ").split()
+    pontos = Pontos(resultado[0], resultado[1])
+    #pontos.oitavas()
+    if i > 8:
+        pontos.quartas()
     
-
-
-    
-       
-    
-    
-
