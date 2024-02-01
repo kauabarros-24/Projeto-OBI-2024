3a287b61025e27978809bff344ad1e3a88f2e14d
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-02-01 17:48:07 -0300

    Com ajuda, porém fui eu

--- Estruturas de dados/copaCerta.py
+++ Estruturas de dados/copaCerta.py
@@ -1,2 +1,20 @@
 from queue import SimpleQueue
+vencedores = [[], [], [], [], []]
+
+vencedores[0] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"]
+q = SimpleQueue()
+
+for fase in range(1, 5):
+    for jogo in range(0, len(vencedores[fase - 1]), 2):
+        equipe1 = vencedores[fase - 1][jogo]
+        equipe2 = vencedores[fase - 1][jogo + 1] 
+        gol1, gol2 = map(int, input().split())
+        if gol1 > gol2:
+            vencedores[fase].append(equipe1)
+        else:
+            vencedores[fase].append(equipe2)
+print(vencedores[4][0])
+        
+
+        
 
