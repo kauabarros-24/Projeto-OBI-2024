4067a67a1937822f5ec0ea7ca94f7fc165307315
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-02-01 19:52:10 -0300

    Quem sou eu

--- Estruturas de dados/cadeia de caracteres.py
+++ Estruturas de dados/cadeia de caracteres.py
@@ -1,19 +1,16 @@
 from queue import SimpleQueue
 repeticoes = int(input())
 q = SimpleQueue()
+
+
+#ProgramaPrincipal
 for i in range(repeticoes):
     expression = input()
     c = 0
     while c < len(expression) - 1:
-        py = len(expression) - 1
-        if expression[c] == "(" and expression[py] == ")" or expression[c] == "{" and expression[py] == "}" or expression[c] == "[" and expression[py] == "]":
-            q.put("S")
-
-        else:
-            q.put("N")
+        
         c+=1
-        py-=1
-
+        
 
 while q.qsize() > 0:
     print(q.get())
