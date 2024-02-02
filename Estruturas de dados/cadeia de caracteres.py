30af583ccfde5b9afca7f6293b327d3d8abea808
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-02-02 13:17:10 -0300

    Kaua

--- Estruturas de dados/cadeia de caracteres.py
+++ Estruturas de dados/cadeia de caracteres.py
@@ -1,12 +1,34 @@
-from queue import SimpleQueue
+from queue import LifoQueue
 #Área de var:
-q = SimpleQueue()
+q = LifoQueue()
 repeticoes = int(input("Digite o numéro de repetições: "))
 lista = []
+o = 0
 
+#Receber ás var's:
 for i in range(repeticoes):
     lista.append(input("Digite a expressão: "))
+
+#Programa principal:    
+for c in lista:
+    if len(c) % 2 != 0:
+        print("N")
+    else:
+        for j in c:
+            for o in c:
+                if j == "(" and o == ")" or j == "[" and o == "]" or j == "{" and o == "}":
+                     q.put("S")
+                else:
+                    q.put("N")
+        print(q.get())
+
+            
     
+
+
+    
+
+
 
 
 
