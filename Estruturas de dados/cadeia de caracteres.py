ca03e0b69e4ddb33273ae01b89cf1c345118100e
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-02-03 17:54:29 -0300

    cadeia de caracteres

--- Estruturas de dados/cadeia de caracteres.py
+++ Estruturas de dados/cadeia de caracteres.py
@@ -1,40 +1,31 @@
 from queue import LifoQueue
-#Área de var:
+
+#área de var
 q = LifoQueue()
-repeticoes = int(input("Digite o numéro de repetições: "))
+repeticoes = int(input("Digite o número de repetições: "))
 lista = []
-o = 0
 
-#Receber ás var's:
+#Programa de apoio
 for i in range(repeticoes):
-    lista.append(input("Digite a expressão: "))
+    lista.append(input("Digite a sequência: "))
 
-#Programa principal:    
+#Programa principal
+
 for c in lista:
+    q = LifoQueue()
     if len(c) % 2 != 0:
         print("N")
     else:
-        for j in c:
-            for o in c:
-                if j == "(" and o == ")" or j == "[" and o == "]" or j == "{" and o == "}":
-                     q.put("S")
+        for o in c:
+            for j in c:
+                if o == "[" and j == "]" or o == "(" and j == ")" or o == "{" and j == "}" :
+                    q.put("S")
+                    c.remive(o)
+                    c.remove(j)
                 else:
                     q.put("N")
         print(q.get())
 
-            
-    
-
-
-    
-
-
-
-
-
-
-
-    
 
     
 
