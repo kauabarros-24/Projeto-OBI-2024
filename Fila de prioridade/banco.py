160467c13fc343958387aa6867dc9641ea2684f3
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-02-10 13:15:32 -0300

    foi

--- Fila de prioridade/banco.py
+++ Fila de prioridade/banco.py
@@ -1,46 +1,38 @@
-import time
 
-atendente, pessoas = map(int, input("Digite o número de atendentes e o número de pessoas: ").split())
+quantidadeCaixas, numeroClintes = map(int, input("Digite o número de caixas e o número de clientes: ").split())
 
 lista = []
-for i in range(pessoas):
-    subLista = []
-    horaChegada, tempoAtendimento = map(int, input("Digite a sua hora de chegada e o tempo que durou o atendimento: ").split())
-    subLista.append(horaChegada)
-    subLista.append(tempoAtendimento)
-    lista.append(subLista)
+for _ in range(numeroClintes):
+    sublista = []
+    tempoChegada, tempoAtendimento = map(int, input("Digite o tempo de chegada e o tempo de atendimento: ").split())
+    sublista.append(tempoChegada)
+    sublista.append(tempoAtendimento)
+    lista.append(sublista)
 
-print(lista)
-
-i, contadorPessoasFora, inicioAtendimento = 0, 0, 0
-lista.append
-
-if atendente == 1:
-    che = 0 
+contador20 = 0
+if quantidadeCaixas == 1: 
+    valorAtendimentoAnteriores = 0
+    todosAtendimentos = 0
     for chegada, atendimento in lista:
-        print(f"Esses são os valores de chegada: {chegada} e de atendimento: {atendimento}")
-        
-        s = atendimento + inicioAtendimento
-        print(f"Esse é o valor para incrementar: {s}")
-        inicioAtendimento+=s
-        print(inicioAtendimento)
-        if chegada > inicioAtendimento:
-            inicioAtendimento = 0
-            print("Zerado")
-        elif inicioAtendimento > 20:
-            contadorPessoasFora+=1
-            print(f"Esse é o contador: {contadorPessoasFora}")
-        
-    
+        ate = todosAtendimentos - chegada
+        todosAtendimentos+= atendimento
+        if ate > 20:
+            contador20+=1
+            print(contador20)
 else:
-    for chegada, atendimento in lista[i::atendente]:
-        s = chegada + atendimento
-        inicioAtendimento+=s
-        if chegada > inicioAtendimento:
-            inicioAtendimento = 0
-        elif inicioAtendimento > 20:
-            contadorPessoasFora+=1   
-    i+=1
-print(contadorPessoasFora)
+    for c in range(quantidadeCaixas):
+        valorAtendimentoAnteriores = 0
+        todosAtendimentos = 0
+        for chegada, atendimento in lista[c - 1:: quantidadeCaixas]:
+            ate = todosAtendimentos - chegada
+            todosAtendimentos+= atendimento
+            if ate > 20:
+                contador20+=1
+                print(contador20)
+        
+
+
+        
+
 
     
