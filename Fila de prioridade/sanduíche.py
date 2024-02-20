812de455bc872b756482210b5a63bedb58cf4019
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-02-20 17:33:01 -0300

    Talvez vá

--- Fila de prioridade/sanduíche.py
+++ Fila de prioridade/sanduíche.py
@@ -1,3 +1,15 @@
 pedacos, tamanhoPedaco = map(int, input().split())
 seq = list(map(int, input().split()))
 
+contador = 0
+for c in range(pedacos):
+    for i in range(i, pedacos):
+        if sum(seq[c:i+1]) <= tamanhoPedaco:
+            contador+=1
+
+for c in range(pedacos):
+    for j in range(i + 1,  pedacos):
+        if sum(tamanhoPedaco[:i]) + sum(tamanhoPedaco[j:]) == tamanhoPedaco:
+            contador+=1
+
+print(contador)
