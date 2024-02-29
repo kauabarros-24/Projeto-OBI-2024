66f976e8890d4ad6b18a29304673a6aebf90be36
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-02-29 14:41:59 -0300

    Maquina

--- Lógica de programação/maquina.py
+++ Lógica de programação/maquina.py
@@ -1,7 +1,18 @@
-andar1, andar2, andar3 = int(input()), int(input()), int(input())
-lista = [andar1, andar2, andar3]
-maiorValor = max(lista)
-#print((andar1) * 2 + (andar3 * 2)) if maiorValor == andar2 else( print(andar1 * 4)))
+andar3, andar2, andar1 = int(input()), int(input()), int(input())
+#lista = [andar3, andar2, andar1]                      
+#-> Solução do Kauã  
+#maiorValor = max(lista)
+#if maiorValor == andar3:
+#    print((andar1 * 4) + (andar2 * 2))
+#elif maiorValor == andar2:
+    #print((andar1 * 2) + (andar3 * 2))
+#else:
+    #print((andar3) * 4 + (andar2 * 2))
+
+um = andar3 * 4 + andar2 * 2
+dois = andar3 * 2 + andar1 * 2
+tres = andar1 * 4 + andar2 * 2
+print(min(um, min(dois, tres)))
 
 
     
