21f82a70f73d353a7ac83844a8dc1199dc964717
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-02-29 14:41:35 -0300

    Maquina

---
+++ Lógica de programação/maquina.py
@@ -0,0 +1,9 @@
+andar1, andar2, andar3 = int(input()), int(input()), int(input())
+lista = [andar1, andar2, andar3]
+maiorValor = max(lista)
+#print((andar1) * 2 + (andar3 * 2)) if maiorValor == andar2 else( print(andar1 * 4)))
+
+
+    
+
+
