2f1d4d48dd05f3d8cf1d7a0d73499e5bdae8a371
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-04 14:49:21 -0300

    exercício de implementação feito, exceto o printar -> Neps

---
+++ Lógica de programação/substituirVetor.py
@@ -0,0 +1,19 @@
+lista = []
+#Ler 10 inteiros
+for _ in range(10):
+    lista.append(int(input()))
+
+#Imprimir o menor valor:
+lista.sort()
+print(f"Menor: {lista.sort[0]}")
+
+#Contar quantas vezes aparece o menor valor
+print(f"Ocorrencias: {lista.count(lista[0])}")
+
+#Substituir o menor valor por -1:
+for i in range(lista.count(lista[0])):
+    lista[i] = -1
+
+#Printar
+
+
