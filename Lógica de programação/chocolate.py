945b3905fbe65fcc13390e8848278390d6e1fdab
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-04 14:14:56 -0300

    Exercício Chocolate -> Resolvido -> OBI 2F -> Neps

--- Lógica de programação/chocolate.py
+++ Lógica de programação/chocolate.py
@@ -1,3 +1,18 @@
-tipoMatriz, valor1, valor2 = int(input()), map(int, input().split()), map(int, input().split())
+z = int(input())
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
 
-    
+def quadrante(x, y, z):
+    if x > z/2 and y > z/2:
+        return 1
+    elif x <= z/2 and y > z/2:
+        return 2
+    elif x <= z/2 and y <= z/2:
+        return 3
+    else:
+        return 4
+   
+if quadrante(x1, y2, z) ==  quadrante(x2, y2, z):
+    print("N")
+else:
+    print("S")
