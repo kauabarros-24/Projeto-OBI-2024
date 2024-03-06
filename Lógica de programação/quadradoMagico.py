0c9f8b9054deb89c83c0afa62933c3e73dd72403
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-06 13:25:14 -0300

    "quadradoMagico -> Função de teste(o teste semp re dá 0) + teste na horizontal -> teste vertical + diagonal e rodar a def de teste "

---
+++ Lógica de programação/quadradoMagico.py
@@ -0,0 +1,14 @@
+#Pegar os dados
+tamanho = int(input())
+listaValores = [list(map(int, input().split())) for _ in range(tamanho)]
+
+#Função para vericifar a soma e valores de apoio:
+valor = 0
+def testeSoma(valorSoma):
+    global valor
+    if valorSoma == sum(listaValores[0]):
+        valor+=1
+soma = 0
+print(valor)
+#Verificar a soma na horizontal:
+for c in range(len(listaValores)): testeSoma(sum(listaValores[c]))
