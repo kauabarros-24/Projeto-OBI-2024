307754151c2b35ea3df117ef49c57a1ca82d82e8
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-07 14:07:57 -0300

    quadradoMagico ->  cálculo para a soma horizontal feito

--- Lógica de programação/quadradoMagico.py
+++ Lógica de programação/quadradoMagico.py
@@ -4,11 +4,14 @@ listaValores = [list(map(int, input().split())) for _ in range(tamanho)]
 
 #Função para vericifar a soma e valores de apoio:
 valor = 0
-def testeSoma(valorSoma):
-    global valor
-    if valorSoma == sum(listaValores[0]):
-        valor+=1
-soma = 0
-print(valor)
+def testeSoma(valorSoma): return valorSoma == sum(listaValores[0])
 #Verificar a soma na horizontal:
-testeSoma(sum(listaValores[c]) for c in range(len(listaValores)))
+valor = 0
+for c in range(len(listaValores)): 
+    if testeSoma(sum(listaValores[c])):
+        valor+=1
+    
+
+
+
+
