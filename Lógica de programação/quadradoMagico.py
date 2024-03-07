eacb94f114d7246da37becfe114249c43c280f51
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-07 14:47:37 -0300

    quadradoMagico - armenamos os N primeiros valores de das sublistas da lista principal

--- Lógica de programação/quadradoMagico.py
+++ Lógica de programação/quadradoMagico.py
@@ -4,11 +4,24 @@ listaValores = [list(map(int, input().split())) for _ in range(tamanho)]
 
 #Função para vericifar a soma e valores de apoio:
 valor = 0
+valorSublista = 0
+count = 0
+listaApoio = []
 def testeSoma(valorSoma): return valorSoma == sum(listaValores[0])
+
 #Verificar a soma na horizontal:
-valor = 0
 valor+= sum(1 for c in range(len(listaValores)) if testeSoma(sum(listaValores[c])))
-print(valor)
+#Vericar a soma na vertical:
+i = 0
+
+for sublista in listaValores:
+    listaApoio.append(sublista[count])
+    if len(listaApoio) == tamanho:
+        count+=1
+        print(listaApoio)
+        testeSoma(sum(listaApoio))
+
+
     
 
 
