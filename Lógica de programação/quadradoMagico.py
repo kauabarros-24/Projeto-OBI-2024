cc324a69ebaa400fc329a36976746b1db988bfd3
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-08 13:19:28 -0300

    quadradoMagico -> terminado - diminuitr as linhas de código

--- Lógica de programação/quadradoMagico.py
+++ Lógica de programação/quadradoMagico.py
@@ -7,6 +7,7 @@ valor = 0
 valorSublista = 0
 count = 0
 listaDiagonal = []
+listaD = []
 def testeSoma(valorSoma): return valorSoma == sum(listaValores[0])
 
 #Verificar a soma na horizontal:
@@ -15,13 +16,15 @@ valor+= sum(1 for c in range(len(listaValores)) if testeSoma(sum(listaValores[c]
 for c in range(len(listaValores)):
     somaColuna = sum(sublista[c] for sublista in listaValores)
     if testeSoma(somaColuna): pass
-#Diagonal
+#Diagonal decrescente
 contador = 0
 for c in range(len(listaValores)):
     listaDiagonal.append(listaValores[c - 1][contador])
     contador+=1
+#Diagonal crescente
+contador = 0
+for c in range(tamanho):
+    listaD.append(listaValores[c - 1][contador])
+    contador+=1
+
 print(testeSoma(sum(listaDiagonal)))
-
-
-    
-    
